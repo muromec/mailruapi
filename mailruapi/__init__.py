efd8561f5a760cc3631486783dba56477a92ebbf
Ilya Petrov <ilya.muromec@gmail.com>
2011-11-23 16:49:42 +0200

    import

---
+++ mailruapi/__init__.py
@@ -0,0 +1,1 @@
+from pymymailru import *
---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name = "mailruapi",
+    version = "0.1",
+    description = 'Mail.ru apps platform api connector',
+    author = 'Ilya Petrov',
+    author_email = 'ilya.muromec@gmail.com',
+    url = 'https://github.com/muromec/',
+    packages = ['mailruapi'],
+    license = "BSD",
+    scripts=[],
+)
