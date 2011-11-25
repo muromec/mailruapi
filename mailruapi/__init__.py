711b5b863ae2abb37c255983a3caa280a74de21e
Denis Chernienko <denekurich@gmail.com>
2011-11-25 17:17:44 +0200

    added new decorate and move in registretion session in there

--- mailruapi/__init__.py
+++ mailruapi/__init__.py
@@ -1,1 +1,19 @@
 from pymymailru import *
+from flask import requst, session, g
+
+def auth(f):
+    from functools import wraps
+
+    @wraps(f)
+    def mailru_auth(*a, **kw):
+
+		g.token = request.args['session_key']
+
+		session['auth'] = {
+			"user_id": request.args['vid'],
+			"token": g.token,
+		}
+
+        return f(*a, **kw)
+
+    return fb_auth
